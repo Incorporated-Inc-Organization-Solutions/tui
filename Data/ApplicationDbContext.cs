fee3c724ef3a12202058bc1816d26e44d3e55287
Alex Jonker <alex.jonker2008@gmail.com>
2026-09-24 10:03:24 +0200

    Setup database connection with a simple User table

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using tui.Models;
+
+namespace tui.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
